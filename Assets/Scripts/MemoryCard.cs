065f8548d7f906a15a9452549d7ed6c1c840fa11
EugeneBogutskiy <bogutskiy1983@gmail.com>
2020-02-23 00:41:05 +0300

    Late update

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    //[SerializeField] private Sprite image;
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController controller;
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+    void Start()
+    {
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf&&controller.canReveal)
+        {
+            cardBack.SetActive(false);
+            controller.CanRevealed(this);
+        }
+        //if (cardBack.activeSelf)
+        //{
+        //    cardBack.SetActive(false);
+        //}
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+    [SerializeField] private Text scoreLabel;
+
+    public const int gridRows = 4;
+    public const int gridCols = 6;
+    public const float offsetX = 2f;
+    public const float offsetY = 2f;
+
+    private MemoryCard _firstRevealed;
+    private MemoryCard _secondRevealed;
+
+    private int _score = 0;
+
+    public bool canReveal
+    {
+        get { return _secondRevealed == null; }
+    }
+
+    public void CanRevealed(MemoryCard card)
+    {
+        if (_firstRevealed == null)
+        {
+            _firstRevealed = card;
+        }
+        else
+        {
+            _secondRevealed = card;
+            StartCoroutine(CheckMatch());
+        }
+    }
+
+    private IEnumerator CheckMatch()
+    {
+        if (_firstRevealed.id == _secondRevealed.id)
+        {
+            _score += 5;
+            scoreLabel.text = "Score: " + _score;
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.5f);
+            _firstRevealed.Unreveal();
+            _secondRevealed.Unreveal();
+            _score--;
+            scoreLabel.text = "Score: " + _score;
+        }
+        _firstRevealed = null;
+        _secondRevealed = null;
+    }
+
+    void Start()
+    {
+        Vector3 startPos = originalCard.transform.position;
+
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11 };
+        numbers = ShuffleArray(numbers);
+
+        for (int i = 0; i < gridCols; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+
+                int index = j * gridCols + i;
+                int id = numbers[index];
+                //int id = Random.Range(0, images.Length);
+                card.SetCard(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+        //int id = Random.Range(0, images.Length);
+        //originalCard.SetCard(id, images[id]);
+    }
+
+    private int[] ShuffleArray(int[] numbers)
+    {
+        int[] newArray = numbers.Clone() as int[];
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+        }
+        return newArray;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void Update()
+    {
+        
+    }
+}
